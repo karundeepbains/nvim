18069d9b55ee5697d038226f286ca6cfb338b6d8
Karundeep Bains <karundeep@bains.io>
2024-11-14 21:13:52 +0000

    add lualine

--- lua/bainsio/packer.lua
+++ lua/bainsio/packer.lua
@@ -33,5 +33,10 @@ return require('packer').startup(function(use)
       'nvim-tree/nvim-tree.lua',
       requires = { 'nvim-tree/nvim-web-devicons' },
     }
+
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
     
 end)
